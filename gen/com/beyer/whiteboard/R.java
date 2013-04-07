d534fb4606f6a900e3ee5abe6854f46fff409a05
Eric Beyer <e.beyer@techsmith.com>
2013-04-07 15:49:13 -0400

    Removed an used files and added some background resources for the buttons on the main screen

--- gen/com/beyer/whiteboard/R.java
+++ gen/com/beyer/whiteboard/R.java
@@ -11,20 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int main_navigation=0x7f020002;
+        public static final int selected=0x7f020003;
     }
     public static final class id {
         public static final int bulletinBoard=0x7f070001;
-        public static final int button1=0x7f070004;
-        public static final int button2=0x7f070005;
-        public static final int button3=0x7f070006;
-        public static final int button4=0x7f070007;
-        public static final int button5=0x7f070008;
-        public static final int button6=0x7f070009;
+        public static final int button1=0x7f070005;
+        public static final int button2=0x7f070006;
+        public static final int button3=0x7f070007;
+        public static final int button4=0x7f070008;
+        public static final int button5=0x7f070009;
+        public static final int button6=0x7f07000a;
+        public static final int classView=0x7f070002;
         public static final int createLesson=0x7f070000;
-        public static final int drawingView=0x7f070002;
-        public static final int linearLayout1=0x7f070003;
-        public static final int menu_settings=0x7f07000a;
+        public static final int drawingView=0x7f070003;
+        public static final int linearLayout1=0x7f070004;
+        public static final int menu_settings=0x7f07000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
